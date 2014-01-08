94f25d9cbe6d5e2ed935be459c421279c8497fa0
RoyAwesome <madKylegar@kylegar.net>
2014-01-07 19:27:49 -0800

    Wrote a quick parser to determine types
    Added all unit types

--- WorldSmith/DataClasses/DotaBaseUnit.cs
+++ WorldSmith/DataClasses/DotaBaseUnit.cs
@@ -8,17 +8,19 @@ using System.Threading.Tasks;
 namespace WorldSmith.DataClasses
 {
     class DotaBaseUnit : DotaDataObject
-    {
+    {     
         [Category("Unit Base")]
-        [Description("Path to the unit's model")]
-        [DefaultValue("")]
+        [Description("No Description Set")]
+        [DefaultValue("models/development/invisiblebox.mdl")]
         public string Model
         {
             get;
             set;
         }
+
+
         [Category("Unit Base")]
-        [Description("Unit's soundset")]
+        [Description("No Description Set")]
         [DefaultValue("")]
         public string SoundSet
         {
@@ -26,8 +28,9 @@ namespace WorldSmith.DataClasses
             set;
         }
 
+
         [Category("Unit Base")]
-        [Description("Unit's Level")]
+        [Description("No Description Set")]
         [DefaultValue(1)]
         public int Level
         {
@@ -37,7 +40,7 @@ namespace WorldSmith.DataClasses
 
 
         [Category("Unit Base")]
-        [Description("Is this unit considered an Ancient in the context of abilities?")]
+        [Description("No Description Set")]
         [DefaultValue(false)]
         public bool IsAncient
         {
@@ -45,8 +48,9 @@ namespace WorldSmith.DataClasses
             set;
         }
 
+
         [Category("Unit Base")]
-        [Description("Is this unit considered a Neutral Unit?")]
+        [Description("No Description Set")]
         [DefaultValue(false)]
         public bool IsNeutralUnitType
         {
@@ -54,8 +58,9 @@ namespace WorldSmith.DataClasses
             set;
         }
 
+
         [Category("Unit Base")]
-        [Description("Units with the same selection group will cycle together when tabbing through owned units")]
+        [Description("No Description Set")]
         [DefaultValue("")]
         public string SelectionGroup
         {
@@ -63,8 +68,9 @@ namespace WorldSmith.DataClasses
             set;
         }
 
+
         [Category("Unit Base")]
-        [Description("Owner selects this unit when it's spawned")]
+        [Description("No Description Set")]
         [DefaultValue(false)]
         public bool SelectOnSpawn
         {
@@ -72,21 +78,579 @@ namespace WorldSmith.DataClasses
             set;
         }
 
+
         [Category("Unit Base")]
-        [Description("Can this unit be dominated")]
+        [Description("No Description Set")]
         [DefaultValue(true)]
         public bool CanBeDominated
         {
             get;
             set;
         }
+
+
         [Category("Unit Base")]
-        [Description("Ignore Add Summoned To Selection")]
+        [Description("No Description Set")]
         [DefaultValue(false)]
         public bool IgnoreAddSummonedToSelection
         {
             get;
             set;
-        }       
+        }
+
+
+        [Category("Unit Base")]
+        [Description("No Description Set")]
+        [DefaultValue(true)]
+        public bool AutoAttacksByDefault
+        {
+            get;
+            set;
+        }
+
+
+        [Category("Unit Base")]
+        [Description("No Description Set")]
+        [DefaultValue("")]
+        public string Ability1
+        {
+            get;
+            set;
+        }
+
+
+        [Category("Unit Base")]
+        [Description("No Description Set")]
+        [DefaultValue("")]
+        public string Ability2
+        {
+            get;
+            set;
+        }
+
+
+        [Category("Unit Base")]
+        [Description("No Description Set")]
+        [DefaultValue("")]
+        public string Ability3
+        {
+            get;
+            set;
+        }
+
+
+        [Category("Unit Base")]
+        [Description("No Description Set")]
+        [DefaultValue("")]
+        public string Ability4
+        {
+            get;
+            set;
+        }
+
+
+        [Category("Unit Base")]
+        [Description("No Description Set")]
+        [DefaultValue("")]
+        public string Ability5
+        {
+            get;
+            set;
+        }
+
+
+        [Category("Unit Base")]
+        [Description("No Description Set")]
+        [DefaultValue("")]
+        public string Ability6
+        {
+            get;
+            set;
+        }
+
+
+        [Category("Unit Base")]
+        [Description("No Description Set")]
+        [DefaultValue("")]
+        public string Ability7
+        {
+            get;
+            set;
+        }
+
+
+        [Category("Unit Base")]
+        [Description("No Description Set")]
+        [DefaultValue("")]
+        public string Ability8
+        {
+            get;
+            set;
+        }
+
+
+        [Category("Unit Base")]
+        [Description("No Description Set")]
+        [DefaultValue(0)]
+        public int ArmorPhysical
+        {
+            get;
+            set;
+        }
+
+
+        [Category("Unit Base")]
+        [Description("No Description Set")]
+        [DefaultValue(false)]
+        public bool MagicalResistance
+        {
+            get;
+            set;
+        }
+
+
+        [Category("Unit Base")]
+        [Description("No Description Set")]
+        [DefaultValue("DOTA_UNIT_CAP_NO_ATTACK")]
+        public string AttackCapabilities
+        {
+            get;
+            set;
+        }
+
+
+        [Category("Unit Base")]
+        [Description("No Description Set")]
+        [DefaultValue(1)]
+        public int AttackDamageMin
+        {
+            get;
+            set;
+        }
+
+
+        [Category("Unit Base")]
+        [Description("No Description Set")]
+        [DefaultValue(1)]
+        public int AttackDamageMax
+        {
+            get;
+            set;
+        }
+
+
+        [Category("Unit Base")]
+        [Description("No Description Set")]
+        [DefaultValue("DAMAGE_TYPE_ArmorPhysical")]
+        public string AttackDamageType
+        {
+            get;
+            set;
+        }
+
+
+        [Category("Unit Base")]
+        [Description("No Description Set")]
+        [DefaultValue(1.7)]
+        public float AttackRate
+        {
+            get;
+            set;
+        }
+
+
+        [Category("Unit Base")]
+        [Description("No Description Set")]
+        [DefaultValue(0.75)]
+        public float AttackAnimationPoint
+        {
+            get;
+            set;
+        }
+
+
+        [Category("Unit Base")]
+        [Description("No Description Set")]
+        [DefaultValue(true)]
+        public bool AttackAcquisitionRange
+        {
+            get;
+            set;
+        }
+
+
+        [Category("Unit Base")]
+        [Description("No Description Set")]
+        [DefaultValue(600)]
+        public int AttackRange
+        {
+            get;
+            set;
+        }
+
+
+        [Category("Unit Base")]
+        [Description("No Description Set")]
+        [DefaultValue(250)]
+        public int AttackRangeBuffer
+        {
+            get;
+            set;
+        }
+
+
+        [Category("Unit Base")]
+        [Description("No Description Set")]
+        [DefaultValue("")]
+        public string ProjectileModel
+        {
+            get;
+            set;
+        }
+
+
+        [Category("Unit Base")]
+        [Description("No Description Set")]
+        [DefaultValue(900)]
+        public int ProjectileSpeed
+        {
+            get;
+            set;
+        }
+
+
+        [Category("Unit Base")]
+        [Description("No Description Set")]
+        [DefaultValue("DOTA_ATTRIBUTE_STRENGTH")]
+        public string AttributePrimary
+        {
+            get;
+            set;
+        }
+
+
+        [Category("Unit Base")]
+        [Description("No Description Set")]
+        [DefaultValue(0)]
+        public int AttributeBaseStrength
+        {
+            get;
+            set;
+        }
+
+
+        [Category("Unit Base")]
+        [Description("No Description Set")]
+        [DefaultValue(0)]
+        public int AttributeStrengthGain
+        {
+            get;
+            set;
+        }
+
+
+        [Category("Unit Base")]
+        [Description("No Description Set")]
+        [DefaultValue(0)]
+        public int AttributeBaseIntelligence
+        {
+            get;
+            set;
+        }
+
+
+        [Category("Unit Base")]
+        [Description("No Description Set")]
+        [DefaultValue(0)]
+        public int AttributeIntelligenceGain
+        {
+            get;
+            set;
+        }
+
+
+        [Category("Unit Base")]
+        [Description("No Description Set")]
+        [DefaultValue(0)]
+        public int AttributeBaseAgility
+        {
+            get;
+            set;
+        }
+
+
+        [Category("Unit Base")]
+        [Description("No Description Set")]
+        [DefaultValue(0)]
+        public int AttributeAgilityGain
+        {
+            get;
+            set;
+        }
+
+
+        [Category("Unit Base")]
+        [Description("No Description Set")]
+        [DefaultValue(0)]
+        public int BountyXP
+        {
+            get;
+            set;
+        }
+
+
+        [Category("Unit Base")]
+        [Description("No Description Set")]
+        [DefaultValue(0)]
+        public int BountyGoldMin
+        {
+            get;
+            set;
+        }
+
+
+        [Category("Unit Base")]
+        [Description("No Description Set")]
+        [DefaultValue(0)]
+        public int BountyGoldMax
+        {
+            get;
+            set;
+        }
+
+
+        [Category("Unit Base")]
+        [Description("No Description Set")]
+        [DefaultValue("DOTA_HULL_SIZE_HERO")]
+        public string BoundsHullName
+        {
+            get;
+            set;
+        }
+
+
+        [Category("Unit Base")]
+        [Description("No Description Set")]
+        [DefaultValue(50)]
+        public int RingRadius
+        {
+            get;
+            set;
+        }
+
+
+        [Category("Unit Base")]
+        [Description("No Description Set")]
+        [DefaultValue(-1)]
+        public int HealthBarOffset
+        {
+            get;
+            set;
+        }
+
+
+        [Category("Unit Base")]
+        [Description("No Description Set")]
+        [DefaultValue("DOTA_UNIT_CAP_MOVE_NONE")]
+        public string MovementCapabilities
+        {
+            get;
+            set;
+        }
+
+
+        [Category("Unit Base")]
+        [Description("No Description Set")]
+        [DefaultValue(300)]
+        public int MovementSpeed
+        {
+            get;
+            set;
+        }
+
+
+        [Category("Unit Base")]
+        [Description("No Description Set")]
+        [DefaultValue(0.5)]
+        public float MovementTurnRate
+        {
+            get;
+            set;
+        }
+
+
+        [Category("Unit Base")]
+        [Description("No Description Set")]
+        [DefaultValue(false)]
+        public bool HasAggressiveStance
+        {
+            get;
+            set;
+        }
+
+
+        [Category("Unit Base")]
+        [Description("No Description Set")]
+        [DefaultValue(100)]
+        public int FollowRange
+        {
+            get;
+            set;
+        }
+
+
+        [Category("Unit Base")]
+        [Description("No Description Set")]
+        [DefaultValue(150)]
+        public int StatusHealth
+        {
+            get;
+            set;
+        }
+
+
+        [Category("Unit Base")]
+        [Description("No Description Set")]
+        [DefaultValue(0)]
+        public int StatusHealthRegen
+        {
+            get;
+            set;
+        }
+
+
+        [Category("Unit Base")]
+        [Description("No Description Set")]
+        [DefaultValue(0)]
+        public int StatusMana
+        {
+            get;
+            set;
+        }
+
+
+        [Category("Unit Base")]
+        [Description("No Description Set")]
+        [DefaultValue(0)]
+        public int StatusManaRegen
+        {
+            get;
+            set;
+        }
+
+
+        [Category("Unit Base")]
+        [Description("No Description Set")]
+        [DefaultValue(-1)]
+        public int StatusStartingMana
+        {
+            get;
+            set;
+        }
+
+
+        [Category("Unit Base")]
+        [Description("No Description Set")]
+        [DefaultValue("DOTA_TEAM_NEUTRALS")]
+        public string TeamName
+        {
+            get;
+            set;
+        }
+
+
+        [Category("Unit Base")]
+        [Description("No Description Set")]
+        [DefaultValue("DOTA_COMBAT_CLASS_ATTACK_HERO")]
+        public string CombatClassAttack
+        {
+            get;
+            set;
+        }
+
+
+        [Category("Unit Base")]
+        [Description("No Description Set")]
+        [DefaultValue("DOTA_COMBAT_CLASS_DEFEND_HERO")]
+        public string CombatClassDefend
+        {
+            get;
+            set;
+        }
+
+
+        [Category("Unit Base")]
+        [Description("No Description Set")]
+        [DefaultValue("DOTA_NPC_UNIT_RELATIONSHIP_TYPE_DEFAULT")]
+        public string UnitRelationshipClass
+        {
+            get;
+            set;
+        }
+
+
+        [Category("Unit Base")]
+        [Description("No Description Set")]
+        [DefaultValue(true)]
+        public bool VisionDaytimeRange
+        {
+            get;
+            set;
+        }
+
+
+        [Category("Unit Base")]
+        [Description("No Description Set")]
+        [DefaultValue(true)]
+        public bool VisionNighttimeRange
+        {
+            get;
+            set;
+        }
+
+
+        [Category("Unit Base")]
+        [Description("No Description Set")]
+        [DefaultValue(1)]
+        public float AttackDesire
+        {
+            get;
+            set;
+        }
+
+
+        [Category("Unit Base")]
+        [Description("No Description Set")]
+        [DefaultValue(false)]
+        public bool HasInventory
+        {
+            get;
+            set;
+        }
+
+
+        [Category("Unit Base")]
+        [Description("No Description Set")]
+        [DefaultValue(true)]
+        public bool WakesNeutrals
+        {
+            get;
+            set;
+        }
+
+
+        [Category("Unit Base")]
+        [Description("No Description Set")]
+        [DefaultValue("")]
+        public string IdleSoundLoop
+        {
+            get;
+            set;
+        }
+
+
+
+
+
     }
 }
--- WorldSmith/Program.cs
+++ WorldSmith/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using WorldSmith.DataClasses;
@@ -10,12 +12,20 @@ namespace WorldSmith
 {
     static class Program
     {
+
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
+
+
+
+
+            //GenerateDataPropertiesFromKeyValues(@"E:\Dota2SDK\root\scripts\npc\npc_units.txt", "npc_dota_units_base", "UnitBase.txt");
+          
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
@@ -33,12 +43,125 @@ namespace WorldSmith
 
             Application.Run(new MainForm());
 
-            
         }
 
         static void Application_ApplicationExit(object sender, EventArgs e)
         {
             DotaData.Shutdown();
+        }
+
+
+        static void GenerateDataPropertiesFromKeyValues(string file, string baseKey, string outputFile)
+        {
+            KVLib.KeyValue kv = KVLib.KVParser.ParseKeyValueText(File.ReadAllText(file));
+            kv = kv[baseKey];
+
+            StringBuilder sb = new StringBuilder();
+            foreach (KVLib.KeyValue k in kv.Children)
+            {
+                sb.AppendLine("[Category(\"Unit Base\")]");
+                sb.AppendLine("[Description(\"No Description Set\")]");
+
+                string type = DetermineType(k);
+
+                sb.Append("[DefaultValue(");
+                if(type == "string")
+                {
+                    sb.Append("\"" + k.GetString() + "\"");
+                }
+                if(type == "int" || type == "float")
+                {
+                    sb.Append(k.GetFloat());
+                }
+                if(type == "bool")
+                {
+                    sb.Append(k.GetBool().ToString().ToLower());
+                }
+                if(type == "CANTDETERMINE")
+                {
+                    sb.Append(k.GetString().ToLower());
+                }
+                sb.AppendLine(")]");
+
+                sb.AppendLine(string.Format("public {0} {1}", type, k.Key));
+                sb.AppendLine("{");
+                sb.AppendLine("\tget;");
+                sb.AppendLine("\tset;");
+                sb.AppendLine("}");
+                sb.AppendLine();
+                sb.AppendLine();
+            }
+
+            File.WriteAllText(outputFile, sb.ToString());
+        }
+
+        static string DetermineType(KVLib.KeyValue k)
+        {
+
+            //If they key contains 'Is', 'Can', 'Has', then it's probably a bool
+            if(k.Key.IndexOf("Is", StringComparison.OrdinalIgnoreCase) != -1 ||
+                k.Key.IndexOf("Can", StringComparison.OrdinalIgnoreCase) != -1 ||
+                k.Key.IndexOf("Has", StringComparison.OrdinalIgnoreCase) != -1)
+            {
+                return "bool";
+            }
+
+            //Attributes are always ints
+            if(k.Key.IndexOf("Attribute", StringComparison.OrdinalIgnoreCase) != -1)
+            {
+                return "int";
+            }
+
+            //Determine the type of the KV
+            List<string> PossibleTypes = new List<string>() {
+                    "float",
+                    "string",
+                    "bool",
+                    "int",
+                };
+
+            float f;
+            if (!k.TryGet(out f)) //Not a float
+            {
+                PossibleTypes.Remove("float");
+            }
+            int i;
+            if (!k.TryGet(out i))
+            {
+                PossibleTypes.Remove("int");
+            }
+            bool b;
+            if (!k.TryGet(out b))
+            {
+                PossibleTypes.Remove("bool");
+            }
+            if (PossibleTypes.Count > 1) //If we have more than one type, it's probably not a string
+            {
+                PossibleTypes.Remove("string");
+            }
+
+            if(PossibleTypes.Count == 1)
+            {
+                //Got it, return that one
+                return PossibleTypes.First();
+            }
+
+            //If the possible types are float or int, default to int
+            if(PossibleTypes.Contains("int") && PossibleTypes.Contains("float") 
+                && !PossibleTypes.Contains("bool"))
+            {
+                return "int";
+            }
+
+            if(k.GetInt() < 0 || k.GetInt() > 1) //If the number is less than 0 or greater than 1, assume int
+            {
+                return "int";
+            }
+            //If the value is '0' or '1', it could be float int or bool.  If the name didn't have 'is', lets default to
+           //lets default to int
+            
+
+            return "CANTDETERMINE";
         }
     }
 }
