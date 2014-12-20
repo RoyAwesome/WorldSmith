a2240176b56fe22f4ba6c1629669fd4721c0283e
Anton Aderum <antonaderum@gmail.com>
2014-12-20 11:11:19 +0100

    start of StartPageForm

---
+++ WorldSmith/Panels/StartPageForm.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace WorldSmith.Panels
+{
+    partial class StartPageForm 
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.recentAddonsListBox = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // recentAddonsListBox
+            // 
+            this.recentAddonsListBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.recentAddonsListBox.FormattingEnabled = true;
+            this.recentAddonsListBox.Location = new System.Drawing.Point(1, 25);
+            this.recentAddonsListBox.Name = "recentAddonsListBox";
+            this.recentAddonsListBox.Size = new System.Drawing.Size(283, 104);
+            this.recentAddonsListBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(-2, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Recent Addons";
+            // 
+            // StartPageForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(583, 286);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.recentAddonsListBox);
+            this.DockAreas = ((DigitalRune.Windows.Docking.DockAreas)((((((DigitalRune.Windows.Docking.DockAreas.Float | DigitalRune.Windows.Docking.DockAreas.Left) 
+            | DigitalRune.Windows.Docking.DockAreas.Right) 
+            | DigitalRune.Windows.Docking.DockAreas.Top) 
+            | DigitalRune.Windows.Docking.DockAreas.Bottom) 
+            | DigitalRune.Windows.Docking.DockAreas.Document)));
+            this.Name = "StartPageForm";
+            this.TabText = "Console";
+            this.Text = "Start Page";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox recentAddonsListBox;
+        private System.Windows.Forms.Label label1;
+
+
+
+    }
+}
---
+++ WorldSmith/Panels/StartPageForm.cs
@@ -0,0 +1,21 @@
+﻿using DigitalRune.Windows.Docking;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WorldSmith.Panels
+{
+    public partial class StartPageForm : DockableForm
+    {
+        public StartPageForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
