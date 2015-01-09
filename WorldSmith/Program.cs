382573d68d7cf54f20fb893ea82e7d4d255e567b
RoyAwesome <kylegar@kylegar.net>
2015-01-08 22:54:37 -0800

    If Worldsmith crashes, write the exception to a file and close

--- WorldSmith/Program.cs
+++ WorldSmith/Program.cs
@@ -24,6 +24,9 @@ namespace WorldSmith
 #if GENERATECLASSES         
             GenerateClasses();
 #else
+            //Set up the crash handler          
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+
 
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
@@ -53,7 +56,7 @@ namespace WorldSmith
             AssetLoadingDialog assets = new AssetLoadingDialog();
             assets.ShowDialog(AssetLoadingDialog.InitialLoad);
 
-          
+
 
             //Construct the main form and load the default project (if any).
             MainForm mainForm = new MainForm();
@@ -63,12 +66,34 @@ namespace WorldSmith
             }
 
             CheckForUpdate.Check(true);
-            
+
             Application.Run(mainForm);
 
             Properties.Settings.Default.Save();
 #endif
-          
+
+        }
+
+        static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            if(e.IsTerminating)
+            {
+                Exception ex = e.ExceptionObject as Exception;
+                WriteCrash(ex.ToString());
+            }
+        }      
+        public static void WriteCrash(string message)
+        {
+            Directory.CreateDirectory("Crashes");
+
+            string header = "Please report this crash to https://github.com/RoyAwesome/worldsmith/issues \n";
+            header += "Please include your project and the build that crashed (located in the Version.txt file)\n\n";
+
+            File.WriteAllText("Crashes/Crash_" + DateTime.Now.ToString("dd-mm-yy_h-mm-ss") +".txt", header + message);
+
+            MessageBox.Show("Worldsmith has crashed.  A crash report has been written to the Crashes/ directory.\nWorldsmith will now close", "Worldsmith Error", MessageBoxButtons.OK);
+            Application.Exit();
+            
         }
 
         static void Application_ApplicationExit(object sender, EventArgs e)
@@ -87,7 +112,7 @@ namespace WorldSmith
         /// </summary>
         public static void GenerateClasses()
         {
-            string outputDir = "../../DataClasses/";            
+            string outputDir = "../../DataClasses/";
             string inputDir = "../../DataSchema/";
 
             string unitDir = outputDir + "ScriptTypes/";
@@ -96,19 +121,19 @@ namespace WorldSmith
             DataSchema.DataClassGenerator.GenerateClassForSchema(inputDir + "UnitSchema.txt", unitDir);
             DataSchema.DataClassGenerator.GenerateClassForSchema(inputDir + "AbilitySchema.txt", unitDir);
             DataSchema.DataClassGenerator.GenerateClassForSchema(inputDir + "ItemSchema.txt", unitDir);
-            DataSchema.DataClassGenerator.GenerateClassForSchema(inputDir + "ModifierSchema.txt", unitDir);            
+            DataSchema.DataClassGenerator.GenerateClassForSchema(inputDir + "ModifierSchema.txt", unitDir);
 
             string actionInputDir = inputDir + "ActionSchemas/";
             string actionOutputDir = outputDir + "ActionTypes/";
 
-            foreach(string file in Directory.GetFiles(actionInputDir))
+            foreach (string file in Directory.GetFiles(actionInputDir))
             {
                 if (!file.EndsWith(".txt")) continue; //If it's not a text file, skip it
 
                 DataSchema.DataClassGenerator.GenerateClassForSchema(file, actionOutputDir);
             }
 
-           
+
         }
     }
 
